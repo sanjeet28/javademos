c855e229fa8e4c7c7509d46d9f8da9d4e18ee7b8
sanjeet91 <you@example.com>
2022-07-14 10:36:40 +0530

    changes

---
+++ 01-helloworld/src/Wrapperclass.java
@@ -0,0 +1,18 @@
+
+public class Wrapperclass {
+
+	public static void main(String[] args) {
+		  int y = 1;
+		  // auto - boxing from int to integer
+		  Integer x = 2;
+		  
+             Integer z = new Integer(5);
+             // autoboxing from integer to int
+             int myint = z;
+             myint = z.intValue();
+              short yshort = 3276;
+              Short newshort = yshort;
+              
+	}
+
+}
---
+++ 01-helloworld/src/s11conditions.java
@@ -0,0 +1,17 @@
+
+public class s11conditions {
+
+	public static void main(String[] args) {
+	boolean isAllen = false;
+	if(isAllen) {
+		System.out.println("It is not an allen");
+		
+	}
+	if(!isAllen) {
+		System.out.println("It is not allen");
+		
+	}
+
+	}
+
+}
